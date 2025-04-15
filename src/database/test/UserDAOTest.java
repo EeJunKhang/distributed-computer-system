6ff4ac13f6544834902259641b6d5467acad53c8
namtung <namtung@namtung>
2025-04-15 11:12:43 +0800

    adjust after pull

--- src/database/test/UserDAOTest.java
+++ src/database/test/UserDAOTest.java
@@ -20,7 +20,7 @@ public class UserDAOTest {
         // Test create
         System.out.println("Testing create user:");
         User newUser = new Customer(
-                1,
+                0,
             "John", 
             "Doe", 
             "johndoe123", 
